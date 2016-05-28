699e29445954f53c69ec262441e6487b1243f37e
riksameidy <riksameidy@gmail.com>
2016-05-28 11:06:58 +0700

    added dosen.java + kelas.java

---
+++ app/src/main/java/riksameidy/com/latihan_bareng1/Dosen.java
@@ -0,0 +1,67 @@
+package riksameidy.com.latihan_bareng1;
+
+/**
+ * Created by ASUS on 28/05/2016.
+ */
+public class Dosen {
+
+    private long nim;
+    private  String name,username,password;
+    private Kelas[] kelas;
+    private int jumlahKelas = 0;
+    private final int MAX_KELAS = 10;
+
+    public Dosen(String name, long nim, String password, String username) {
+        this.name = name;
+        this.nim = nim;
+        this.password = password;
+        this.username = username;
+    }
+
+    public Kelas getKelas(int n){
+        return kelas[n];
+    }
+
+    public void addKelas(int jumlahMhs, int kodekelas, String namaKelas, String namaMK){
+        if(jumlahKelas<MAX_KELAS){
+            kelas[jumlahKelas] = new Kelas(jumlahMhs,kodekelas,namaKelas,namaMK);
+            jumlahKelas++;
+        }
+    }
+
+    public int getJumlahKelas(){
+        return jumlahKelas;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getNim() {
+        return nim;
+    }
+
+    public void setNim(long nim) {
+        this.nim = nim;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
