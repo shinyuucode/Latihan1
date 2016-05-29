dbd4ce6d0adbb5f629f25ae5849e4b228727a3ec
amalyacitra <amalyacitra@gmail.com>
2016-05-29 12:29:05 +0700

    making  editprofil, addkelas,pilihkelas,kelolakelasdosen. EditProfil is done

---
+++ app/src/main/java/riksameidy/com/latihan_bareng1/KelolaKelasDosen.java
@@ -0,0 +1,13 @@
+package riksameidy.com.latihan_bareng1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KelolaKelasDosen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kelola_kelas_dosen);
+    }
+}
