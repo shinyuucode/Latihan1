271b1b5da0b8ad8e920a50e18076c4c9836079a8
amalyacitra <amalyacitra@gmail.com>
2016-06-04 22:39:15 +0700

    making kelola kelas dosen include info kelas, kelola mahasiswa, kelompok. kelola tugas not yet. sign up java completed

---
+++ app/src/main/java/riksameidy/com/latihan_bareng1/DosenTambahKelompokBaru.java
@@ -0,0 +1,13 @@
+package riksameidy.com.latihan_bareng1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DosenTambahKelompokBaru extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dosen_tambah_kelompok_baru);
+    }
+}
